46db4f96c3081fc2e79e4c0a1b2fb46385a79923
BOUKRANI Mohamed <mohamedboukrani7@gmail.com>
2026-03-23 22:54:59 +0100

    Coding...

--- src/processing/chunking.py
+++ src/processing/chunking.py
@@ -3,7 +3,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def get_chunks(documents):
     print("CHUNKING...")                       
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 200, chunk_overlap = 0)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 200, chunk_overlap = 50)
     
     chunks = []
     for doc in documents:
